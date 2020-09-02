0f9ac48b2389f13d936f9216b08ee1e20337c155
Administrator <admin@example.com>
2020-09-02 20:13:44 +0530

    Py added

---
+++ hello.py
@@ -0,0 +1,10 @@
+print('Helloworld')
+a=10
+b=20
+c=a+b
+print(c)
+if (a > b):
+    print('Hello',a)
+else:
+    print('World',b)
+
---
+++ table.py
@@ -0,0 +1,4 @@
+num = 4
+print("Multiplaction of number is ",num)
+for i in range(1, 11):
+    print(num,"X",i,"=",num * i)
